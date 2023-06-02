33969d35bd8dc4f701dd6546eb9dff83a2555ebf
corinB <gkdisrha2020gmail.com>
2023-06-02 21:57:13 +0900

    도전학기

--- server.js
+++ server.js
@@ -1,82 +1,107 @@
-const express =require('express');
+const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const Db = require('mongodb/lib/db');
-const MongoClient = require('mongoDB').MongoClient;
-const methodoverride = require('method-override')
-app.use(methodoverride('_method'))
+const { MongoClient } = require('mongodb');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 
-
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 var db;
-MongoClient.connect('mongodb+srv://qor:0989@ais.smqldtl.mongodb.net/?retryWrites=true&w=majority', function(err, client){
-    if (err) return console.log(err);
-    db = client.db('test');
+MongoClient.connect('mongodb+srv://qor:0989@ais.smqldtl.mongodb.net/?retryWrites=true&w=majority', function (err, client) {
+  if (err) return console.log(err);
+  db = client.db('test');
 
-    app.listen('3000', function(){
-      console.log('listening on 3000')
-    });
-  })
-
-let multer = require('multer');
-var storage = multer.diskStorage({
-
-  destination : function(req, file, cb){
-    cb(null, 'imege')
-  },
-  filename : function(req, file, cb){
-    cb(null, file.originalname )
-  }
-
+  app.listen('3000', function () {
+    console.log('listening on 3000');
+  });
 });
 
-
-app.use(bodyParser.urlencoded({extended : true}));
-
-var path = require('path');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const dir = 'image';
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    cb(null, dir);     
+  },
+  filename: function (req, file, cb) {
+    const newFileName = req.body.title + path.extname(file.originalname);
+    cb(null, newFileName);
+  }
+});
 
 var upload = multer({
-    storage: storage,
-    fileFilter: function (req, file, callback) {
-        var ext = path.extname(file.originalname);
-        if(ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
-            return callback(new Error('PNG, JPG만 업로드하세요'))
-        }
-        callback(null, true)
-    },
-    limits:{
-        fileSize: 1024 * 1024
+  storage: storage,
+  fileFilter: function (req, file, callback) {
+    var ext = path.extname(file.originalname);
+    if (ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
+      return callback(new Error('PNG, JPG만 업로드하세요'));
     }
+    callback(null, true);
+  },
+  limits: {
+    fileSize: 1024 * 1024
+  }
 });
 
 app.use(express.static(path.join(__dirname, 'front/build')));
 
-app.post('/add',upload.single('image'),function(res,req){
-    console.log(res.body) 
-    db.collection('counter').findOne({name:'w_list'}, function(err, result){
-        var w_count = result.no
+app.post('/add', upload.single('image'), function(req, res) {
+  console.log(req.body); // 요청의 본문(body) 출력
+  console.log(req.file); // 업로드된 파일 정보 출력
 
-        db.collection("list").insertOne(
-          { _id: w_count + 1, 제목: res.body.title, 내용: res.body.data },
+  // 파일 정보 확인
+  if (!req.file) {
+    console.log('파일이 업로드되지 않았습니다.');
+  } else {
+    // 업로드된 파일의 경로 확인
+    console.log('업로드된 파일 경로:', req.file.path);
+  }
+
+
+  // 이미지 파일은 req.file에서 사용 가능
+  // 나머지 데이터는 req.body에서 사용 가능
+
+  db.collection('counter').findOne({ name: '계시물갯수' }, function (err, result) {
+    var w_count = result.totalpost;
+    db.collection('qor').insertOne(
+      {
+        _id: w_count + 1,
+        제목: req.body.title,
+        내용: req.body.data,
+        날짜1: req.body.day1,
+        날짜2: req.body.day2,
+        이미지 : req.file.path
+      },
+      function (err, result) {
+        console.log('저장완료');
+        db.collection('counter').updateOne(
+          { name: '계시물갯수' },
+          { $inc: { totalpost: 1 } },
           function (err, result) {
-            console.log("저장완료");
-            db.collection("counter").updateOne(
-              { name: "w_list" },
-              { $inc: { no: 1 } },
-              function (err, result) {
-                if (err) {
-                  return console.log(err);
-                }
-              }
-            );
+            if (err) {
+              return console.log(err);
+            }
           }
         );
-    });
-    req.send('업로드완료')
+      }
+    );
+  });
+
+  res.send('업로드완료');
 });
 
+
+
 // 항상 아래쪽에 놓아야함
-app.get('/', function (res, req) {
-    req.sendFile(path.join(__dirname, '/front/build/index.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/front/build/index.html'));
+
+  db.collection('qor').find().toArray(function(err, result){
+    console.log(result);
+    res.render('PostFilter.jsx', {posts : result}); 
   });
+  
+});
